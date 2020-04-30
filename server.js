f60688f7241911c42336092f5fedf0503c571440
Alois Carrera <aloiscarrera@gmail.com>
2020-04-30 15:48:42 -0500

    Retrieving data from db
    Simple response in json from table "products"

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
+
+app.get('/products', async (req, res)=> {
+    try {
+        const conn = pool.getConn();
+        const query = 'select * from products';
+        const rows = await conn.query(query);
+        res.status(200).json(rows);
+    } catch (e) {
+        console.log(e)        
+    }
+})
 
 app.listen(3000, () =>{
     console.log('Server in port ', app.get('port'));
