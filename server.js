096c89b96016914d77c54bc159f93eb27434941c
Alois Carrera <aloiscarrera@gmail.com>
2020-04-30 20:04:48 -0500

    Sending JSON to user
    User recieve a JSON as response.
    
    Added "await" for query execution, this may be the most helpful code line.

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const pool = require('./db');
 
+app.listen(3000, () =>{
+    console.log('Server on port', 3000);
+})
+
 app.get('/products', async (req, res)=> {
+    let conn;
     try {
-        const conn = pool.getConn();
+        const conn = await pool.getConnection();
         const query = 'select * from products';
         const rows = await conn.query(query);
         res.status(200).json(rows);
     } catch (e) {
-        console.log(e)        
+        throw e
+    } finally {
+        if (conn) return conn.release();
     }
-})
-
-app.listen(3000, () =>{
-    console.log('Server in port ', app.get('port'));
-})
+});
